ab12c45946f4cc68b4eef1d446f463a3d1bb9532
Daniel Chernokalov <danil-chernokalov88@outlook.com>
2019-05-08 08:36:59 +0300

    time

--- automation/deploy.sh
+++ automation/deploy.sh
@@ -10,8 +10,8 @@ git clone git@github.com:Danielchernokalov88/isbn_scrapy.git
 
 # 
 echo yes | cp isbn_scrapy/abe/isbn/settings.prod.py isbn_scrapy/abe/isbn/settings.py 
-#rm isbn_scrapy/abe/isbn/settings.prod.py
-#rm isbn_scrapy/abe/isbn/settings.dev.py
+rm isbn_scrapy/abe/isbn/settings.prod.py
+rm isbn_scrapy/abe/isbn/settings.dev.py
 
 # remove deploy script
 rm isbn_scrapy/automation/deploy.sh
@@ -22,6 +22,8 @@ rm -rf isbn_scrapy/.git
 tar czvf isbn_scrapy.tgz isbn_scrapy
 
 readonly MY_PEM="/Users/xiong/Documents/projects/scrapping/server_info/xiaogangEC2.pem"
+#
+
 # deploy
 scp -i "$MY_PEM" isbn_scrapy.tgz centos@34.200.219.71:~/
 ssh -i "$MY_PEM" centos@34.200.219.71 'tar xzf isbn_scrapy.tgz;source venv/bin/activate;pwd;cd isbn_scrapy;automation/run_abe.sh;'
@@ -32,5 +34,5 @@ ssh -i "$MY_PEM" centos@34.200.219.71 'tar xzf isbn_scrapy.tgz;source venv/bin/a
 # git archive --format=tar.gz -o ./build/isbn.tar.gz master
 
 
-#cd ..
-#rm -rf build
+cd ..
+rm -rf build
--- automation/run_abe.sh
+++ automation/run_abe.sh
@@ -6,3 +6,7 @@ nohup scrapy crawl abe  > /dev/null 2>&1 & echo $! > abe.pid
 
 echo "running abe"
 
+now=$(date +"%Y%m%d")
+filename = "abe_$now.log"
+echo "log file name is $filename"
+tail -f abe/$filename
